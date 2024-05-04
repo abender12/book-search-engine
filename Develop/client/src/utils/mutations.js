a1562c5c98d63da80f134b4db09731c0e19eb97c
Alicen Bender <alicene.bender@gmail.com>
2024-05-04 15:29:05 -0400

    Added information for queries and mutations, created apollo server in app.jsx

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,78 @@
+// Apollo Client
+import { gql } from '@apollo/client';
+
+// Create a user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+`;
+
+// Login as an existing user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+
+// Save book
+export const SAVE_BOOK = gql`
+  mutation saveBook($book: SavedBookInput!) {
+    saveBook(book: $book) {
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+
+// Remove book
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,23 @@
+// Import apollo server
+import { gql } from '@apollo/client';
+
+// Retreives books
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
